98652ef7030a42095097643de4ec74075e10c6d1
kinnalrutu-ops <kinnalrutu@gmail.com>
2025-10-30 14:31:17 +0530

    save

---
+++ swapp.py
@@ -0,0 +1,5 @@
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+print("Before swapping: num1 =", num1, "num2 =", num2)
+num1, num2 = num2, num1
+print("After swapping: num1 =", num1, "num2 =", num2)
