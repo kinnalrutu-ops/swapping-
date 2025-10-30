d94db82a31b049d556b87835b328997c5fb9225f
kinnalrutu-ops <kinnalrutu@gmail.com>
2025-10-30 14:33:51 +0530

    save

--- swapp.py
+++ swapp.py
@@ -3,3 +3,7 @@ num2=int(input("Enter second number: "))
 print("Before swapping: num1 =", num1, "num2 =", num2)
 num1, num2 = num2, num1
 print("After swapping: num1 =", num1, "num2 =", num2)
+temp=a
+a=b
+b=temp
+print("After swapping using temp variable: a =", a, "b =", b)
